0c65ee15e2705351c59efd0d7e0f978b8d765d8b
Horace Williams <horace.d.williams@gmail.com>
2016-03-14 12:43:33 -0600

    add mod 1 homework calendar link

--- lib/calendar_collection.rb
+++ lib/calendar_collection.rb
@@ -7,6 +7,7 @@ class CalendarCollection
   attr_reader :calendars
 
   CALENDARS = [
+    "casimircreative.com_isoih6973tpoq1i8mf3nqs3mno@group.calendar.google.com",
     "casimircreative.com_59k8msrrc2ddhcv787vubvp0s4@group.calendar.google.com",
     "casimircreative.com_rps2hg1nfqjih4rcl3gl6s4lpk@group.calendar.google.com",
     "casimircreative.com_e9k9b6n7bok174ilmqbfdr0sc4@group.calendar.google.com",
