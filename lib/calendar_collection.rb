2faff25232bdb37d2c875ea45b59b45819914961
ian douglas <ian.douglas@iandouglas.com>
2019-09-11 09:00:06 -0600

    Gemfile update and labeling BE calendars

--- lib/calendar_collection.rb
+++ lib/calendar_collection.rb
@@ -8,10 +8,19 @@ class CalendarCollection
 
   CALENDARS = [
     # "casimircreative.com_isoih6973tpoq1i8mf3nqs3mno@group.calendar.google.com",
+
+    # BE Mod 1:
     # "casimircreative.com_59k8msrrc2ddhcv787vubvp0s4@group.calendar.google.com",
-    "casimircreative.com_rps2hg1nfqjih4rcl3gl6s4lpk@group.calendar.google.com",
+
+    # BE Mod 2:
+    # "casimircreative.com_rps2hg1nfqjih4rcl3gl6s4lpk@group.calendar.google.com",
+
+    # BE Mod 3
     # "casimircreative.com_e9k9b6n7bok174ilmqbfdr0sc4@group.calendar.google.com",
-    # "casimircreative.com_r9jfiq9f37h6rdt2s8ssofss4k@group.calendar.google.com"
+
+    # BE Mod 4:
+    "casimircreative.com_r9jfiq9f37h6rdt2s8ssofss4k@group.calendar.google.com"
+
   ]
 
   def initialize
